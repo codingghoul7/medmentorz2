ee912b5fecfaebc1704eda2197fa1ab0f2005f81
CodingGhoul <108980953+codingghoul7@users.noreply.github.com>
2023-08-28 11:06:44 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,65 @@
+const express =require("express");
+const path= require("path");
+require("../src/db/conn");
+const hbs =require("hbs");
+
+
+
+
+const connectDB = require('../src/db/conn');
+
+
+
+
+const app = express();
+const port = process.env.PORT || 3000
+
+
+//setting the path
+const staticpath = path.join(__dirname,"../public")
+const templatepath = path.join(__dirname,"../templates/views")
+const partialpath = path.join(__dirname,"../templates/partials")
+
+// console.log(( path.join(__dirname,"../public"))) 
+
+//middleware
+app.use("/css", express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")));
+app.use("/jq", express.static(path.join(__dirname,"../node_modules/jquery/dist")));
+app.use(express.static(staticpath));
+
+//fixed error
+// app.set("views",path.join(__dirname,"templates/views"))
+// console.log(templatepath)
+app.set("views",templatepath)
+hbs.registerPartials(partialpath)
+
+
+
+// app.set("views",path.join(__dirname,"templates/partials")) 
+// app.set("views",partialpath)
+
+
+app.set("view engine", "hbs")
+
+
+
+
+
+//routing
+app.get("/",(req,res)=>{
+    res.render("index")
+    // res.send("hello")
+    // console.log(index)
+    })
+    
+    app.get("/contact",(req,res)=>{
+        res.render("contact")
+        
+        // console.log(index)
+        })
+    
+    //server create
+    app.listen(port, ()=>{
+        console.log(`server is running at port:"${port}`)
+    })
---
+++ src/db/conn.js
@@ -0,0 +1,27 @@
+const mongoose = require ("mongoose");
+
+//creating a database
+
+//  mongoose.connect("mongodb://localhost:27017/Medmentorz",{
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// }).then(()=>{
+//     console.log("connection successful")
+// }) .catch((error) => {console.log(error)});
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`mongodb://localhost:27017/medmentorz`, {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: {conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+}
+
+
+module.exports = connectDB;
